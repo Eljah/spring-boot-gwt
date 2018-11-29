491759ee65679706746f59d9aff076a4c3af46ae
Eljah <eljah@mail.ru>
2018-11-29 17:50:59 +0300

    making possibke both gwt:run and spring-boot:run with the same behavior adding olproxy and openlayers-gwt-server

--- src/main/java/ru/integration/com/client/ui/component/MapPanel.java
+++ src/main/java/ru/integration/com/client/ui/component/MapPanel.java
@@ -1,15 +1,19 @@
 package ru.integration.com.client.ui.component;
 
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import org.gwtopenmaps.openlayers.client.*;
-import org.gwtopenmaps.openlayers.client.control.LayerSwitcher;
+import org.gwtopenmaps.openlayers.client.control.*;
 import org.gwtopenmaps.openlayers.client.feature.VectorFeature;
 import org.gwtopenmaps.openlayers.client.geometry.Collection;
 import org.gwtopenmaps.openlayers.client.geometry.Geometry;
 import org.gwtopenmaps.openlayers.client.geometry.Point;
-import org.gwtopenmaps.openlayers.client.layer.OSM;
-import org.gwtopenmaps.openlayers.client.layer.Vector;
+import org.gwtopenmaps.openlayers.client.layer.*;
+import org.gwtopenmaps.openlayers.client.protocol.WFSProtocol;
+import org.gwtopenmaps.openlayers.client.protocol.WFSProtocolOptions;
+import org.gwtopenmaps.openlayers.client.strategy.BBoxStrategy;
+import org.gwtopenmaps.openlayers.client.strategy.Strategy;
 import ru.integration.com.client.ui.MainPanel;
 
 /**
@@ -31,10 +35,104 @@ public class MapPanel extends FlowPanel {
     MapPanel(String height, String width) {
         this.height = height;
         this.width = width;
+
+//        OpenLayers.setProxyHost("olproxy?targetURL=");
+//
+//        //create some MapOptions
+//        MapOptions defaultMapOptions = new MapOptions();
+//        defaultMapOptions.setNumZoomLevels(16);
+//
+//        //Create a MapWidget
+//        MapWidget mapWidget = new MapWidget("500px", "500px", defaultMapOptions);
+//        //Create a WMS layer as base layer
+//        WMSParams wmsParams = new WMSParams();
+//        wmsParams.setFormat("image/png");
+//        wmsParams.setLayers("topp:tasmania_state_boundaries");
+//        wmsParams.setStyles("");
+//
+//        WMSOptions wmsLayerParams = new WMSOptions();
+//        wmsLayerParams.setUntiled();
+//        wmsLayerParams.setTransitionEffect(TransitionEffect.RESIZE);
+//
+//        String wmsUrl = "http://localhost:8181/geoserver/wms";
+//
+//        WMS wmsLayer = new WMS("Basic WMS", wmsUrl, wmsParams, wmsLayerParams);
+//
+//        //Add the WMS to the map
+//        Map map = mapWidget.getMap();
+//        map.addLayer(wmsLayer);
+//
+//        //Create a WFS layer
+//        WFSProtocolOptions wfsProtocolOptions = new WFSProtocolOptions();
+//        wfsProtocolOptions.setUrl("http://localhost:8181/geoserver/wfs");
+//        wfsProtocolOptions.setFeatureType("tasmania_roads");
+//        wfsProtocolOptions.setFeatureNameSpace("http://www.openplans.org/topp");
+//        //if your wms is in a different projection use wfsProtocolOptions.setSrsName(LAMBERT72);
+//
+//        WFSProtocol wfsProtocol = new WFSProtocol(wfsProtocolOptions);
+//
+//        VectorOptions vectorOptions = new VectorOptions();
+//        vectorOptions.setProtocol(wfsProtocol);
+//        vectorOptions.setStrategies(new Strategy[]{new BBoxStrategy()});
+//        //if your wms is in a different projection use vectorOptions.setProjection(LAMBERT72);
+//
+//
+//
+//        Vector wfsLayer = new Vector("wfsExample", vectorOptions);
+//        map.addLayer(wfsLayer);
+//
+//        //Create some styles for the wfs
+//        final Style normalStyle = new Style(); //the normal style
+//        normalStyle.setStrokeWidth(3);
+//        normalStyle.setStrokeColor("#FF0000");
+//        normalStyle.setFillColor("#FFFF00");
+//        normalStyle.setFillOpacity(0.8);
+//        normalStyle.setStrokeOpacity(0.8);
+//        final Style selectedStyle = new Style(); //the style when a feature is selected, or temporaly selected
+//        selectedStyle.setStrokeWidth(3);
+//        selectedStyle.setStrokeColor("#FFFF00");
+//        selectedStyle.setFillColor("#FF0000");
+//        selectedStyle.setFillOpacity(0.8);
+//        selectedStyle.setStrokeOpacity(0.8);
+//        final StyleMap styleMap = new StyleMap(normalStyle, selectedStyle,
+//                selectedStyle);
+//        wfsLayer.setStyleMap(styleMap);
+//
+//        //Create a ModifyFeature control that enables WFS modification
+//        final ModifyFeatureOptions modifyFeatureControlOptions = new ModifyFeatureOptions();
+//        modifyFeatureControlOptions.setMode(ModifyFeature.RESHAPE); //options are RESHAPE, RESIZE, ROTATE and DRAG
+//        final ModifyFeature modifyFeatureControl = new ModifyFeature(wfsLayer,
+//                modifyFeatureControlOptions);
+//
+//        map.addControl(modifyFeatureControl);
+//        modifyFeatureControl.activate();
+
+        /*
+         * Note that for saving back to the WFS you can use
+         * final SaveStrategy saveStrategy = new SaveStrategy();
+         * saveStrategy.setAuto(true);
+         * vectorOptions.setStrategies(new Strategy[] {new BBoxStrategy(), saveStrategy }); // (instead of only BBOXStrategy)
+         */
+
+//        //Lets add some default controls to the map
+//        map.addControl(new LayerSwitcher()); //+ sign in the upperright corner to display the layer switcher
+//        map.addControl(new OverviewMap()); //+ sign in the lowerright to display the overviewmap
+//        map.addControl(new ScaleLine()); //Display the scaleline
+//
+//        //Center and zoom to a location
+//        map.setCenter(new LonLat(146.7, -41.8), 6);
+//
+//
+//        mapWidget.getElement().getFirstChildElement().getStyle().setZIndex(0); //force the map to fall behind popups
+
+
         MapOptions defaultMapOptions = new MapOptions();
         mapWidget = new MapWidget(width + "", height + "", defaultMapOptions);
         OSM osmMapnik = OSM.Mapnik("Mapnik");
         OSM osmCycle = OSM.CycleMap("CycleMap");
+
+        //WMS etomesto=new WMS("http://map.etomesto.ru/kazan/1887/");
+        //http://map.etomesto.ru/kazan/1887/
 
         osmMapnik.setIsBaseLayer(true);
         osmCycle.setIsBaseLayer(true);
--- src/main/java/ru/integration/com/server/ServerApplication.java
+++ src/main/java/ru/integration/com/server/ServerApplication.java
@@ -1,10 +1,13 @@
 package ru.integration.com.server;
 
+import org.gwtopenmaps.openlayers.server.GwtOpenLayersProxyServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -15,6 +18,11 @@ import org.springframework.core.env.Environment;
 public class ServerApplication {
 
 	final static Logger logger = LoggerFactory.getLogger(ServerApplication.class);
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean(){
+		return new ServletRegistrationBean(new GwtOpenLayersProxyServlet(),"/olproxy/*");
+	}
 
 	@Autowired
 	Environment env;
